ca17a83da60cf070d4fe6268467e34f74af48284
Jarosław Leśniak <jarek9244@gmail.com>
2019-08-08 14:19:47 +0200

    Initial update

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Configure from './components/configure';
+
+export default function App() {
+  return (
+    <Configure/>
+  );
+}
---
+++ frontend/components/configure.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+class Configure extends React.Component {
+    render() {
+        return (
+            <Text>Konfiguracja</Text>
+        );
+    }
+}
+
+export default Configure;
