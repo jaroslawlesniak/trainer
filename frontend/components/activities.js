062beb5358f43f89ca067adf0dc8519729cf34f5
Jarosław Leśniak <jarek9244@gmail.com>
2019-08-23 21:50:12 +0200

    Added modal inputs scheme

--- frontend/components/activities.js
+++ frontend/components/activities.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 import { Modal, View, AsyncStorage, Text, StyleSheet, ToolbarAndroid } from 'react-native';
 import Page from '../libs/page';
+import { TextInput } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
     item: {
@@ -11,6 +12,14 @@ const styles = StyleSheet.create({
         borderBottomColor: '#eee',
         borderBottomWidth: 1,
         padding: 15
+    },
+    header: {
+
+    },
+    input: {
+        padding: 5,
+        backgroundColor: '#f2f2f2',
+        borderRadius: 10
     }
 });
 
@@ -29,7 +38,13 @@ export default class Activities extends React.Component {
                 {title: 'Podciąganie', days: [0,1,2,3,4,5,6], finished: true},
                 {title: 'Hantle', days: [0,1,2,3,4,5,6], finished: false},
             ],
-            page: Page.ACTIVITIES
+            page: Page.ACTIVITIES,
+            new_activity: {
+                title: '',
+                break_time: 0,
+                days: [false, false, false, false, false, false, false],
+                series: []
+            }
         }
 
         // this.state = {
@@ -55,8 +70,12 @@ export default class Activities extends React.Component {
         if(this.state.page === Page.NEW_ACTIVITY) {
             return (
             <Modal animationType="fade" transparent={false} onRequestClose={() => {this.setState({page: Page.ACTIVITIES})}}>
-                <View>
-                    <Text>Work</Text>
+                <View style={{padding: 10}}>
+                    <Text style={styles.header}>Nazwa ćwiczenia</Text>
+                    <TextInput style={styles.input} onChangeText={(e) => {this.handleTitleChange(e)}}/>
+                    <Text style={styles.header}>Czas pomiędzy seriami</Text>
+                    <TextInput style={styles.input} onChangeText={(e) => {this.handleBreakTimeCHange(e)}}/>
+                    <Text style={styles.header}>Dni tygodnia</Text>
                 </View>
             </Modal>);
         }
@@ -87,5 +106,21 @@ export default class Activities extends React.Component {
                 page: Page.NEW_ACTIVITY
             });
         }
+    }
+
+    handleTitleChange(title) {
+        let modyfiedActivity = this.state.new_activity;
+        modyfiedActivity.title = title;
+        this.setState({
+            new_activity: modyfiedActivity
+        });
+    }
+
+    handleBreakTimeCHange(time) {
+        let modyfiedActivity = this.state.new_activity;
+        modyfiedActivity.break_time = time;
+        this.setState({
+            new_activity: modyfiedActivity
+        });
     }
 }
